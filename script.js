5c30e1c12af99cec21697855e406a1ba2fec6a25
NamLikestoCode <namhn7777@gmail.com>
2022-12-18 21:30:42 -0800

    Finished U.I with working game, need to add game round limit

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-console.log('Hello');
 
 let playerwins = 0;
 let computerwins = 0;
 let gameties = 0;
 let gamesPlayed= 0;
 
-const options = ['rock','paper','scissors'];
 
-function getComputerChoice(options){
+
+function getComputerChoice(){   
+    const options = ['rock','paper','scissors'];
     const random = options[Math.floor(Math.random() * options.length)];
-    console.log(`Computer chose ${random}`);
+    // console.log(`Computer chose ${random}`);
     return random;
 }
 
 
-function getPlayerChoice() {
-    let askPlayer = prompt('What is your choice?',)
-    console.log('Players turn')
-    console.log(`Player chose ${askPlayer}`);
-    return askPlayer.toLowerCase();
-}
-
-const playerSelection = getPlayerChoice();
-
-const computerSelection = getComputerChoice(options);
+// function getPlayerChoice() {
+//     //     let askPlayer = prompt('What is your choice?',)
+//     //     console.log('Players turn')
+//     //     console.log(`Player chose ${askPlayer}`);
+//     //     return askPlayer.toLowerCase();
+//     }
+    
+    
+    
+//     const playerSelection = getPlayerChoice();
+//     // = getPlayerChoice();
+    
+    const computerSelection = getComputerChoice();
 
 function playround(playerSelection,computerSelection){
-    if(playerSelection == computerSelection){
-        console.log('It is a tie');
+    
+        if(playerSelection == computerSelection){  
         gameties += 1;
         gamesPlayed += 1;
         console.log(`Tie Score: ${gameties}`);
+        const results = document.querySelector(".Results");
+        results.innerText = `You tied! You both picked ${playerSelection}`;
+        
+        let gameTies = document.querySelector("#gameties");
+        gameTies.innerText = `Game Ties: ${gameties}`;
+        
+
     }else if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
               (playerSelection == 'scissors' && computerSelection == 'paper') || 
               (playerSelection == 'paper' && computerSelection == 'rock')){
@@ -38,27 +48,77 @@ function playround(playerSelection,computerSelection){
                 playerwins += 1;
                 gamesPlayed += 1;
                 console.log(`Player Score: ${playerwins}`);
+                const results = document.querySelector(".Results");
+        results.innerText = `You WON! You picked ${playerSelection} while computer chose ${computerSelection}`;
+
+
+
+        let playerscore = document.querySelector("#playerscore");
+        playerscore.innerText = ` Player Score: ${playerwins}`;
+      
+        
     } else {console.log('Computer wins')
             computerwins += 1;
             gamesPlayed += 1;
-            console.log(`Computer Score ${computerwins}`);    }
+            console.log(`Computer Score ${computerwins}`);
+            const results = document.querySelector(".Results");
+            results.innerText = `You lose! You picked ${playerSelection} while computer chose ${computerSelection}`;   
+
+
+         
+        let computerScore = document.querySelector("#computerscore");
+        computerScore.innerText = `Computer Score: ${computerwins}`;
+       
+        
+         }
 }
 
-const firstRound = playround(playerSelection,computerSelection);
+// const firstRound = playround(playerSelection,computerSelection);
 
 function game(){
     console.log('Game Start');
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; gamesPlayed < 5; i++){
         
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice(options);
-
+        playround();
         console.log(playround(playerSelection,computerSelection));        
-        
+        console.log(`Player Wins ${playerwins}`,`Computer Wins ${computerwins}`, `Game Ties: ${gameties}`)
 
     }
 }
 
-game();
+// game();
+
+let rockButton = document.querySelector(".rockbutton");
+
+let paperbutton = document.querySelector(".paperbutton");
+
+let scissorsbutton = document.querySelector(".scissorsbutton")
+
+const computerChoicediv = document.querySelector("ComputersChoice");
+
+const results = document.querySelector(".Results")
+
+rockButton.addEventListener("click", () => {
+    const playerSelection = "rock";   
+    const computerSelection = getComputerChoice();
+    console.log("rock");
+    playround(playerSelection,computerSelection)
+})
+
+paperbutton.addEventListener("click", () => {
+    const playerSelection = "paper";   
+    const computerSelection = getComputerChoice();
+    console.log("paper");
+    playround(playerSelection,computerSelection)
+})
+
+scissorsbutton.addEventListener("click", () => {
+    const playerSelection = "scissors";   
+    const computerSelection = getComputerChoice();
+    console.log("scissors");
+    playround(playerSelection,computerSelection)
+})
+
+
 
 
